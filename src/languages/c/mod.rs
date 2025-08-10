603fd2193b1837b94a2749ec06128d5a2c78fab4
Nicolas <nicolasmbdandanell@gmail.com>
2025-08-10 21:25:17 +0200

    Implemented source files and parser array for encoding/decoding

--- src/languages/c/mod.rs
+++ src/languages/c/mod.rs
@@ -1,9 +1,10 @@
 mod header;
+mod parser;
 mod runic_definitions;
 mod source;
 mod utilities;
 
-use crate::languages::c::{ utilities::CConfigurations, header::output_header, runic_definitions::output_runic_definitions };
+use crate::languages::c::{ utilities::CConfigurations, header::output_header, parser::output_parser, runic_definitions::output_runic_definitions, source::output_source };
 use crate::{ Configurations, RuneFileDescription };
 use std::path::Path;
 
@@ -12,10 +13,24 @@ pub fn output_c_files(file_descriptions: Vec<RuneFileDescription>, output_path: 
     let c_configurations: CConfigurations = CConfigurations::parse(&file_descriptions, &configurations);
 
     // Create runic definitions file
-    output_runic_definitions(&c_configurations, output_path);
+    println!("Outputting runic definitions");
+    output_runic_definitions(&file_descriptions, &c_configurations, output_path);
 
-    for file in file_descriptions {
+    // Create source and header files matching the Rune files
+    println!("Outputting headers and sources for:");
+    for file in &file_descriptions {
+        println!("    {0}.rune", file.file_name);
+
         // Create header file
-        output_header(file, output_path);
+        output_header(&file, output_path);
+
+        // Create source file
+        output_source(&file, output_path);
     }
+
+    // Create parser
+    println!("Outputting parser file");
+    output_parser(&file_descriptions, output_path);
+
+    println!("Done!");
 }
--- src/languages/c/runic_definitions.rs
+++ src/languages/c/runic_definitions.rs
@@ -1,4 +1,4 @@
-use crate::languages::c::utilities::{ CConfigurations, OutputFile };
+use crate::{ languages::c::utilities::{ pascal_to_uppercase, spaces, CConfigurations, OutputFile }, types::StructDefinition, RuneFileDescription };
 use std::path::Path;
 
 fn type_from_size(size: usize) -> String {
@@ -11,11 +11,12 @@ fn type_from_size(size: usize) -> String {
     }
 }
 
-pub fn output_runic_definitions(configurations: &CConfigurations, output_path: &Path) {
+pub fn output_runic_definitions(file_descriptions: &Vec<RuneFileDescription>, configurations: &CConfigurations, output_path: &Path) {
     let definitions_file_string: String = format!("{0}/runic_definitions.h", output_path.to_str().unwrap());
 
-    let     enum_attributes:     String = String::with_capacity(0x100);
     let mut bitfield_attributes: String = String::with_capacity(0x100);
+    let     enum_attributes:     String = String::with_capacity(0x100);
+    let mut parser_attributes:   String = String::with_capacity(0x100);
     let mut struct_attributes:   String = String::with_capacity(0x100);
 
     // Parse "packed" attribute
@@ -30,6 +31,12 @@ pub fn output_runic_definitions(configurations: &CConfigurations, output_path: &
     // Enums have backing types, and do not need to be packed
 
     if configurations.compiler_configurations.pack {
+        // Parser
+        match parser_attributes.is_empty() {
+            true  => parser_attributes.push_str("packed"),
+            false => parser_attributes.push_str(", packed")
+        }
+
         // Structs
         match struct_attributes.is_empty() {
             true  => struct_attributes.push_str("packed"),
@@ -40,7 +47,15 @@ pub fn output_runic_definitions(configurations: &CConfigurations, output_path: &
     // Parse "section" attribute
     // ——————————————————————————
 
-    // For variables, and not data definitions...
+    if configurations.compiler_configurations.section != None {
+        let section_name: String = configurations.compiler_configurations.section.clone().unwrap();
+
+        // Parser
+        match parser_attributes.is_empty() {
+            true  => parser_attributes.push_str(format!("section(\"{0}\")", section_name).as_str()),
+            false => parser_attributes.push_str(format!(", section(\"{0}\")", section_name).as_str())
+        }
+    }
 
     // Create attribute strings
     // —————————————————————————
@@ -54,13 +69,40 @@ pub fn output_runic_definitions(configurations: &CConfigurations, output_path: &
         false => format!("__attribute__(({0}))", enum_attributes)
     };
 
+    // Parser
+    let runic_parser_string: String = match parser_attributes.is_empty() {
+        true  => String::new(),
+        false => format!("__attribute__(({0}))", parser_attributes)
+    };
+
     // Structs
     let runic_struct_string: String = match struct_attributes.is_empty() {
         true  => String::new(),
         false => format!("__attribute__(({0}))", struct_attributes)
     };
 
+    // Create a list with all declared structs across all files
+    let mut struct_definitions: Vec<StructDefinition> = Vec::with_capacity(0x40);
+
+    for file in file_descriptions {
+        if !file.definitions.structs.is_empty() {
+            struct_definitions.append(&mut file.definitions.structs.clone());
+        }
+    }
+
+    // Sort the list alphabetically
+    struct_definitions.sort_by(|a, b| a.name.to_ascii_uppercase().cmp(&b.name.to_ascii_uppercase()));
+
+    // Create output file
     let mut definitions_file: OutputFile = OutputFile::new(definitions_file_string);
+
+    // Disclaimers
+    // ————————————
+
+    // ...
+
+    // Definitions
+    // ————————————
 
     definitions_file.add_line(format!("#ifndef RUNE_DEFINITIONS_H"));
     definitions_file.add_line(format!("#define RUNE_DEFINITIONS_H"));
@@ -70,11 +112,11 @@ pub fn output_runic_definitions(configurations: &CConfigurations, output_path: &
     definitions_file.add_line(format!("// ———————————————————"));
     definitions_file.add_newline();
 
-    definitions_file.add_line(format!("#define FIELD_INDEX_BITS         0x1F"));
-    definitions_file.add_line(format!("#define LACKS_VERIFICATION_FIELD INT8_MIN"));
-    definitions_file.add_line(format!("#define NO_PARSER                0"));
-    definitions_file.add_line(format!("#define TRANSPORT_TYPE_BITS      0xE0"));
-    definitions_file.add_line(format!("#define VERIFICATION_FIELD       0x1F"));
+    definitions_file.add_line(format!("#define FIELD_INDEX_BITS      0x1F"));
+    definitions_file.add_line(format!("#define NO_VERIFICATION_FIELD INT8_MIN"));
+    definitions_file.add_line(format!("#define NO_PARSER             0"));
+    definitions_file.add_line(format!("#define TRANSPORT_TYPE_BITS   0xE0"));
+    definitions_file.add_line(format!("#define VERIFICATION_FIELD    0x1F"));
     definitions_file.add_newline();
 
     definitions_file.add_line(format!("// Configuration dependent definitions"));
@@ -86,6 +128,7 @@ pub fn output_runic_definitions(configurations: &CConfigurations, output_path: &
 
     definitions_file.add_line(format!("#define RUNIC_BITFIELD {0}", runic_bitfield_string));
     definitions_file.add_line(format!("#define RUNIC_ENUM     {0}", runic_enum_string));
+    definitions_file.add_line(format!("#define RUNIC_PARSER   {0}", runic_parser_string));
     definitions_file.add_line(format!("#define RUNIC_STRUCT   {0}", runic_struct_string));
     definitions_file.add_newline();
 
@@ -100,6 +143,36 @@ pub fn output_runic_definitions(configurations: &CConfigurations, output_path: &
     definitions_file.add_line(format!("#define FIELD_OFFSET_TYPE {0}", type_from_size(configurations.field_offset_type_size)));
     definitions_file.add_line(format!("#define MESSAGE_SIZE_TYPE {0}", type_from_size(configurations.message_size_type_size)));
     definitions_file.add_line(format!("#define PARSER_INDEX_TYPE {0}", type_from_size(configurations.parser_index_type_size)));
+    definitions_file.add_newline();
+
+    definitions_file.add_line(format!("// Parsing array definitions"));
+    definitions_file.add_line(format!("// ——————————————————————————"));
+    definitions_file.add_newline();
+
+    definitions_file.add_line(format!("/** First entry of the parser array is purposely empty, as NO_PARSER is the index 0 for quick comparison check */"));
+    definitions_file.add_line(format!("#define PARSER_COUNT {0}", struct_definitions.len() + 1));
+    definitions_file.add_newline();
+
+    // Calculate longest struct name for spacing reasons
+    let mut longest_struct_name: usize = 0;
+
+    for struct_definition in &struct_definitions {
+        let struct_name: String = pascal_to_uppercase(&struct_definition.name);
+
+        if struct_name.len() > longest_struct_name {
+            longest_struct_name = struct_name.len();
+        }
+    }
+
+    for i in 0..struct_definitions.len() {
+        let struct_name: String = pascal_to_uppercase(&struct_definitions[i].name);
+
+        definitions_file.add_line(format!("#define {0}_INDEX {1}{2}",
+            struct_name,
+            spaces(longest_struct_name - struct_name.len()),
+            i + 1
+        ));
+    }
     definitions_file.add_newline();
 
     definitions_file.add_line(format!("#endif // RUNIC_DEFINITIONS_H"));
